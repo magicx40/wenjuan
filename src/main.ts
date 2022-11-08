5bd33c5ec87f7dcd36dbe49b136e0367e2f442ab
hanzongyuan <hanzongyuan@foxmail.com>
2022-11-09 00:07:02 +0800

    naive setup

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { createApp } from 'vue'
 import '@/assets/css/normalize.css'
 import App from '@/App.vue'
-import Router from '@/router/index'
+import router from '@/router/index'
+import {
+    // create naive ui
+    create,
+    // component
+    NButton
+} from 'naive-ui'
 
+const naive = create({
+    components: [NButton]
+})
 const app = createApp(App);
 
-app.use(Router)
+app.use(router)
+app.use(naive)
 
 app.mount('#app')
