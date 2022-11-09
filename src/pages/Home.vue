9350cd177ae2cffb93b3c1f0ed07ed62896cdb14
hanzongyuan <hanzongyuan@foxmail.com>
2022-11-09 08:16:23 +0800

    update

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,24 +1,32 @@
 <template>
     <div class="page_home">
-        <n-carousel effect="card" prev-slide-style="transform: translateX(-150%) translateZ(-800px);"
-            next-slide-style="transform: translateX(50%) translateZ(-800px);" style="height: 240px" :show-dots="false">
-            <n-carousel-item :style="{ width: '60%' }">
-                <img class="carousel-img"
-                    src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg">
-            </n-carousel-item>
-            <n-carousel-item :style="{ width: '60%' }">
-                <img class="carousel-img"
-                    src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg">
-            </n-carousel-item>
-            <n-carousel-item :style="{ width: '60%' }">
-                <img class="carousel-img"
-                    src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg">
-            </n-carousel-item>
-            <n-carousel-item :style="{ width: '60%' }">
-                <img class="carousel-img"
-                    src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg">
-            </n-carousel-item>
-        </n-carousel>
+        <div class="carousel_box">
+            <n-carousel 
+                effect="card"
+                prev-slide-style="transform: translateX(-150%) translateZ(-200px);"
+                next-slide-style="transform: translateX(50%) translateZ(-200px);" 
+                style="height: 400px;" 
+                :show-dots="true"
+                show-arrow
+            >
+                <n-carousel-item :style="{ width: '60%' }">
+                    <img class="carousel-img"
+                        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel1.jpeg">
+                </n-carousel-item>
+                <n-carousel-item :style="{ width: '60%' }">
+                    <img class="carousel-img"
+                        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel2.jpeg">
+                </n-carousel-item>
+                <n-carousel-item :style="{ width: '60%' }">
+                    <img class="carousel-img"
+                        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel3.jpeg">
+                </n-carousel-item>
+                <n-carousel-item :style="{ width: '60%' }">
+                    <img class="carousel-img"
+                        src="https://naive-ui.oss-cn-beijing.aliyuncs.com/carousel-img/carousel4.jpeg">
+                </n-carousel-item>
+            </n-carousel>
+        </div>
     </div>
 </template>
 <script setup lang='ts'>
